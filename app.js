50f014f458a3cfa74169567e3b27a7bad37385f1
Nithin-Shetty22 <nithinshetty388@gmail.com>
2024-08-21 14:17:16 +0530

    first commit

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express();
 
 // Define routes and middleware here
 // ...
-app.get('/', (req, res)=>{
-    res.status(200);
-    res.send("Welcome to root URL of Server");
-});
+app.get("/user/:id/:start/:end", function(req, res){ 
+
+    const user_id = req.params['id'] 
+    const start = req.params['start'] 
+    const end = req.params['end'] 
+    
+    console.log("User ID :",user_id);
+    console.log("Start :",start);
+    console.log("End :",end);
+    res.send(JSON.stringify({"User ID":user_id,"Start":start,"End":end}));
+   }) 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
